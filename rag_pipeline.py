7744d5cbbbbc8bbf1b5565c00f949951058914e8
manish1008iitr <m.k.dangi99@gmail.com>
2026-09-10 00:10:51 -0700

    Made strcutual changes in RAG_pipeline

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -3,47 +3,45 @@ from langchain_core.prompts import ChatPromptTemplate
 from config import Config
 config_data = Config() 
 
-from vectorstore import Vector_loading
+from vector_store import Vector_loading
 vector_loader = Vector_loading()
 
-class prompt_make:
-    def __init__(self, context):
-        self.question = " "
-        self.context  = context
-        self.format_promt = " "
-
-    def prompt_making():
-        question = input("Please type your question here \n ")
-        self.prompt = ChatPromptTemplate.from_template("""
-                    Answer the question using ONLY the context provided below.
-                    If the answer is not present in the context, say:
-                    "I don't know based on the PDF."
-                    Context:
-                    {context}
-                    Question:
-                    {question}
-                    Answer:
-                    """
-        )
-        format_prompt = prompt.invoke(
-        {
-            "context": self.context, 
-            "question": self.question
-        }
-    )
-        return self.format_prompt
-
-class RAGPipeline(prompt_make):
-    def __init__(self):
+class RAGPipeline():
+    def __init__(self, retriever):
         self.llm = ChatOllama(
             model = config_data.LLM,
             temperature = 0
         )
-    
-    def create_answer():
-        response = self.llm.invoke(
-            
+
+        self.retriever = retriever
+        self.prompt = ChatPromptTemplate.from_template("""
+                Answer the question using ONLY the context provided below.
+                If the answer is not present in the context, say:
+                "I don't know based on the PDF."
+
+                Context: {context}
+                Question: {question}
+                Answer: """)
+
+    def create_answer(self, question):
+        documents = self.retriever.invoke(question)
+        
+        ##combining retrieved texts
+        context = "\n\n".join(
+        document.page_content for document in documents
         )
+
+        ## Generating prompt 
+        format_prompt = self.prompt.invoke({
+            "context":context
+            "question": question
+        })
+
+        ##Generating response 
+        response = self.llm.invoke(
+            self.format_prompt
+        )
+        return response.context
 
 
 
